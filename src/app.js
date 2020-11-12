8cef0231d1ca562fd7734230ef9353872ce45deb
Alejandro G <imthejano@outlook.com>
2020-11-12 14:18:14 -0600

    reestructuracion de api

--- src/app.js
+++ src/app.js
@@ -23,37 +23,23 @@ io.on('connection', ( socket )=> {
 		var user = null
 		token.auth = {...args}
 		switch (app) {
-			//socket is from a nivel client
-			case 'nivel_client': {
-				//add token to user when socket connect
+			case 'level_client': {
 				try {
 					var {data} = await axios.get(process.env.NIVEL_API + 'user/me', {headers})
 					user = data.user
 					if(user) user = await axios.put(process.env.NIVEL_API + 'user/me/add/rotosockettoken/'+socket.conn.id, {}, {headers})
 				} catch (error) {console.error('error')}
-				//remove token from user when socket disconnect
 				socket.on('disconnect', async ()=> {
-					var res = await axios.put(process.env.NIVEL_API + 'user/rotosockettoken/remove/'+socket.conn.id, {}, { headers: {auth: process.env.NIVEL_API_KEY} })
-				})
-				//nivel socket events
-				socket.on('nivel:new_sfdata', async (args)=> {
-					try {
-						var {data} = await axios.get(process.env.NIVEL_API + 'user/me', {headers})
-						user = data.user
-						if(user) user = await axios.put(process.env.NIVEL_API + 'user/me/add/rotosockettoken/'+socket.conn.id, {}, {headers})
-					} catch (error) {console.error('error')}
+					await axios.put(process.env.NIVEL_API + 'user/rotosockettoken/remove/'+socket.conn.id, {}, { headers: {auth: process.env.NIVEL_API_KEY} })
 				})
 			} break;
-			//socket is from nivel api client
-			case 'nivel_api': {
+			case 'level_api': {
 				try {
 					var {data} = await axios.get(process.env.NIVEL_API + 'user/me', {headers})
 					user = data.user
 					if(user.email == 'rotosocket@nivel.com'){
-						console.log('api conected to socket: ');
-						console.log(socket);
-						socket.on('nivel:new_sfdata', (args)=> {
-							io.to(args.to).emit('nivel:new_sfdata', args.message)
+						socket.on('level:new_sfdata', (args)=> {
+							io.to(args.to).emit('level:new_sfdata', args.message)
 						})
 					}
 				} catch (error) {console.error('error')}
